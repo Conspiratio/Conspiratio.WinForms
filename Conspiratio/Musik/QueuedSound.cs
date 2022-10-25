31039088d351e8ec80045980134aceab8780fbfa
SirTobyB <baldur_angbant@web.de>
2022-10-25 16:52:20 +0200

    Erste Version einer SoundQueue hinzugefügt, mit POCO-Klasse für den Sound in der Queue (wird benötigt, um mehrere Sound direkt hinter- oder leicht versetzt während einander abspielen zu können)

---
+++ Conspiratio/Musik/QueuedSound.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Conspiratio.Musik
+{
+    public enum SoundType
+    {
+        Effect = 0,
+        Voice = 1
+    }
+
+    /// <summary>
+    /// POCO class for a sound to be played in a queue
+    /// </summary>
+    public class QueuedSound
+    {
+        /// <summary>
+        /// Create a new sound for the queue
+        /// </summary>
+        /// <param name="sound">Sound as stream (example: from a .wav file from the resources)</param>
+        /// <param name="soundType">OPTIONAL: The sound type, it's necessary for getting the default volume from the setting, if the volume was not passed as a parameter)</param>
+        /// <param name="volumeInPercent">OPTIONAL: The sound volume in percent (1 - 100 are allowed), it if's not passed, it will be readed from the settings (for the given sound type)</param>
+        /// <param name="startMillisecondsEarlier">OPTIONAL: The milliseconds, this sound will be played earlier (while the sound before is still playing). This will only taken into account, if
+        /// there is a sound before this sound in the queue.
+        /// Example: When the sound before this sound has a length of 6000 milliseconds and we say, 
+        /// this sound should start, when the sound before has played 4000 milliseconds, then we have to set 2000 as a value for this parameter.
+        /// This sound will then played 2000 milliseconds earlier then normal (normal is just after the sound before is completely played) and will
+        /// start at 4000 milliseconds of the sound before this sound in the queue. THis is useful, if the pause between to sounds are to long and we want to shortend it (for voice acting for example).
+        /// </param>
+        public QueuedSound(Stream sound, SoundType soundType = SoundType.Effect, int volumeInPercent = 0, int startMillisecondsEarlier = 0)
+        {
+            Sound = sound;
+            SoundType = soundType;
+
+            if (volumeInPercent == 0)
+            {
+                // get the volume from the settings
+                switch (soundType)
+                {
+                    case SoundType.Effect:
+                        VolumeInPercent = Convert.ToInt32(Properties.Settings.Default["Sound_Lautstaerke"]); ;
+                        break;
+                    case SoundType.Voice:
+                        VolumeInPercent = 85;  // TODO: get from new setting
+                        break;
+                }
+            }
+            else
+                VolumeInPercent = volumeInPercent;
+
+            StartMillisecondsEarlier = startMillisecondsEarlier;
+        }
+
+        public Stream Sound { get; private set; }
+        public int VolumeInPercent { get; private set; }
+        public int StartMillisecondsEarlier { get; private set; }
+        public SoundType SoundType { get; private set; }
+    }
+}
---
+++ Conspiratio/Musik/SoundQueuePlayer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Timers;
+
+using NAudio.Wave;
+
+namespace Conspiratio.Musik
+{
+    public class SoundQueuePlayer
+    {
+        private Timer _timerNextSound;  // TODO: If we can get rid of this, this class can be made static, because then it's stateless :) (maybe a job for future refactoring)
+
+        /// <summary>
+        /// Play all Sounds from Queue in the order of being added to the queue (first added, first played and so on) and clear the queue.
+        /// </summary>
+        /// <param name="soundQueue">The queue which should be played</param>
+        public void PlayAllSoundsFromQueue(List<QueuedSound> soundQueue)
+        {
+            if (soundQueue.Count == 0)
+                return;
+
+            PlayNextSoundFromQueue(soundQueue);
+        }
+
+        private void PlayNextSoundFromQueue(List<QueuedSound> soundQueue)
+        {
+            if (soundQueue.Count == 0)
+                return;
+
+            var queueSound = soundQueue.First();
+            var lengthOfSound = PlaySoundFromQueue(queueSound.Sound, queueSound.VolumeInPercent, soundQueue);
+
+            soundQueue.Remove(queueSound);
+
+            var nextSound = soundQueue.FirstOrDefault();
+
+            if (nextSound == null)
+                return;
+
+            if (nextSound.StartMillisecondsEarlier > 0)
+            {
+                // clean up timer
+                if (_timerNextSound != null)
+                {
+                    _timerNextSound.Stop();
+                    _timerNextSound.Dispose();
+                }
+
+                _timerNextSound = new Timer(lengthOfSound.TotalMilliseconds - nextSound.StartMillisecondsEarlier);
+                _timerNextSound.Elapsed += (object source, ElapsedEventArgs e) => PlayNextSoundFromQueue(soundQueue);
+                _timerNextSound.AutoReset = false;
+                _timerNextSound.Enabled = true;
+            }
+        }
+
+        private TimeSpan PlaySoundFromQueue(Stream sound, int volumeInPercent, List<QueuedSound> soundQueue)
+        {
+            if (sound == null)
+                return TimeSpan.Zero;
+
+            IWavePlayer wavePlayerSoundFromQueue = new WaveOutEvent();
+
+            if (soundQueue.Count <= 1)
+                wavePlayerSoundFromQueue.PlaybackStopped += wavePlayerSoundFromQueue_LastSound_PlaybackStopped;
+            else
+                wavePlayerSoundFromQueue.PlaybackStopped += wavePlayerSoundFromQueue_PlaybackStopped;
+
+            WaveStream sourceStreamSounds = new WaveFileReader(sound);
+            WaveChannel32 inputStreamSounds = new WaveChannel32(sourceStreamSounds);
+
+            inputStreamSounds.Volume = (float)Convert.ToDouble(volumeInPercent) / 100;
+            inputStreamSounds.PadWithZeroes = false;
+
+            var lengthOfSound = inputStreamSounds.TotalTime;
+
+            wavePlayerSoundFromQueue.Init(inputStreamSounds);
+            wavePlayerSoundFromQueue.Play();
+
+            return lengthOfSound;
+        }
+
+        private void wavePlayerSoundFromQueue_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            if (sender is WaveOutEvent)
+            {
+                // clean up wave player
+                var wavePlayer = sender as WaveOutEvent;
+
+                wavePlayer.Stop();
+                wavePlayer.Dispose();
+            }
+        }
+
+        private void wavePlayerSoundFromQueue_LastSound_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            wavePlayerSoundFromQueue_PlaybackStopped(sender, e);
+
+            // clean up timer
+            if (_timerNextSound != null)
+            {
+                _timerNextSound.Stop();
+                _timerNextSound.Dispose();
+            }
+        }
+    }
+}
